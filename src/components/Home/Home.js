95f5cb28742686c023680cc4ea268f3939a7e9ec
Vijayaperumal L <160581852+Vijay-0414@users.noreply.github.com>
2025-09-05 07:18:05 +0530

    Update Home.js

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -22,7 +22,7 @@ function Home() {
 
               <h1 className="heading-name">
                 I'M
-                <strong className="main-name"> VIJAYAPERUMAL L</strong>
+                <strong className="main-name"> Arjun</strong>
               </h1>
 
               <div style={{ padding: 50, textAlign: "left" }}>
